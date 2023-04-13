9b95ba8d54a1f69cbd26362937be38c28c6def3d
Anton Sherpaev <asherpaev@odin.com>
2023-04-13 23:21:46 +0700

    sprint 1 finals A in progress

--- app/src/main/java/algorithms/sprint1/finals/A.java
+++ app/src/main/java/algorithms/sprint1/finals/A.java
@@ -3,10 +3,28 @@ package algorithms.sprint1.finals;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class A {
 
     public static String calculateShortestWay(String streetLength, String housesNumbers) {
+
+        //0 1 4 9 0
+
+        var numbers = Arrays.stream(housesNumbers.split(" "))
+                .map(Integer::parseInt)
+                .toList()
+                .toArray(new Integer[Integer.parseInt(streetLength)]);
+        var shortestWay = new ArrayList<Integer>();
+        int numbersIncrease = 0;
+        int numbersDecrease = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 0) {
+
+            }
+        }
         return "";
     }
     public static void main(String[] args) throws IOException {
