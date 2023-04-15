517a784b163d7fc2002ce8428c428d593911ae43
Anton Sherpaev <asherpaev@odin.com>
2023-04-16 00:24:21 +0700

    sprint 1 finals A in progress

--- app/src/main/java/algorithms/sprint1/finals/A.java
+++ app/src/main/java/algorithms/sprint1/finals/A.java
@@ -5,28 +5,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class A {
 
     public static String calculateShortestWay(String streetLength, String housesNumbers) {
-
-        //0 1 4 9 0
-
         var numbers = Arrays.stream(housesNumbers.split(" "))
                 .map(Integer::parseInt)
                 .toList()
                 .toArray(new Integer[Integer.parseInt(streetLength)]);
-        var shortestWay = new ArrayList<Integer>();
-        int numbersIncrease = 0;
-        int numbersDecrease = 0;
+        var shortestWay = new ArrayList<String>();
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == 0) {
-
+                shortestWay.add("0");
+            }
+            else {
+                shortestWay.add(String.valueOf(calculateMinimalDelta(i, numbers)));
             }
         }
-        return "";
+        return String.join(" ", shortestWay);
     }
+
+    private static int calculateMinimalDelta(int nonZeroIndex, Integer[] numbers) {
+        int wayToZeroRight = 0;
+        for (int i = nonZeroIndex; i < numbers.length; i++) {
+            if (numbers[i] == 0) {
+                break;
+            }
+            wayToZeroRight++;
+        }
+        int wayToZeroLeft = 0;
+        for (int i = nonZeroIndex; i >= 0; i--) {
+            if (numbers[i] == 0) {
+                break;
+            }
+            wayToZeroLeft++;
+        }
+        return Math.min(wayToZeroLeft, wayToZeroRight);
+    }
+
     public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             var streetLength = reader.readLine();
--- app/src/test/java/algorithms/sprint1/finals/ATest.java
+++ app/src/test/java/algorithms/sprint1/finals/ATest.java
@@ -12,4 +12,10 @@ class ATest {
         var shortestWay = calculateShortestWay("5", "0 1 4 9 0");
         assertEquals("0 1 2 1 0", shortestWay);
     }
+
+    @Test
+    void test2() {
+        var shortestWay = calculateShortestWay("6", "0 7 9 4 8 20");
+        assertEquals("0 1 2 3 4 5", shortestWay);
+    }
 }
