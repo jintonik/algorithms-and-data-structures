0086784b36e4da98dcbe6131531e56bf3aafba10
Anton Sherpaev <asherpaev@odin.com>
2023-04-16 17:58:12 +0700

    sprint 1 finals A another approach but still TL

--- app/src/main/java/algorithms/sprint1/finals/A.java
+++ app/src/main/java/algorithms/sprint1/finals/A.java
@@ -8,29 +8,25 @@ import java.util.ArrayList;
 public class A {
 
     public static String calculateShortestWay(String streetLength, String housesNumbers) {
-        var numbers = housesNumbers.split(" ");
-        var shortestWay = new ArrayList<String>();
+        var numbers = housesNumbers.split(" ");  // 0, 1, 4, 9, 0
+        var shortestWay = new ArrayList<String>();              // 0, 1, 2, 1, 0
+        var zeroIndexes = new ArrayList<Integer>();             // 0, 4
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i].equals("0")) {
-                shortestWay.add("0");
+                zeroIndexes.add(i);
             }
-            else {
-                int counter = 0;
-                while (counter <= numbers.length) {
-                    counter++;
-                    if (i - counter >= 0) {
-                        if (numbers[i - counter].equals("0")) {
-                            shortestWay.add(String.valueOf(counter));
-                            break;
-                        }
-                    }
-                    if (i + counter < numbers.length) {
-                        if (numbers[i + counter].equals("0")) {
-                            shortestWay.add(String.valueOf(counter));
-                            break;
-                        }
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i].equals("0")){
+                shortestWay.add("0");
+            } else {
+                int diff = Math.abs(i - zeroIndexes.get(0));
+                for (Integer zeroIndex : zeroIndexes) {
+                    if (Math.abs(i - zeroIndex) < diff) {
+                        diff = Math.abs(i - zeroIndex);
                     }
                 }
+                shortestWay.add(String.valueOf(diff));
             }
         }
         return String.join(" ", shortestWay);
