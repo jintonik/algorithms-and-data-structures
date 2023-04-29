142f278073bd8f46157f803a6380bc3316057127
Anton <anton.sherpaev@gmail.com>
2023-04-29 12:31:39 +0700

    sprint 1 A done

--- app/src/main/java/algorithms/sprint1/finals/A.java
+++ app/src/main/java/algorithms/sprint1/finals/A.java
@@ -1,67 +1,40 @@
 package algorithms.sprint1.finals;
 
 import java.io.*;
-import java.util.ArrayList;
 
 public class A {
 
     public static String calculateShortestWay(String streetLength, String housesNumbers) {
-        var numbers = housesNumbers.split(" ");  // 0, 1, 4, 9, 0
-        var stringBuilder = new StringBuilder();       // 0, 1, 2, 1, 0
+        var numbers = housesNumbers.split(" ");
+        var stringBuilder = new StringBuilder();
         int housesNumber = Integer.parseInt(streetLength);
-        int zeroDistance = 0;                          //98 0 10 77 0 59 28 0 94
-        for (int i = 0; i < housesNumber; i++) {       // 0 1  2  3 4  5  6 7  8
+        int previousZeroIndex = -1;
+        for (int i = 0; i < housesNumber; i++) {
             int left;
             int right;
-            int previousZeroIndex = i - zeroDistance;
-            if (numbers[i].equals("0") && previousZeroIndex == 0) {
-                for (int j = 0; j <= i; j++) {
-                    stringBuilder.append(Math.abs(i - j));
-                    stringBuilder.append(" ");
+            if (numbers[i].equals("0")) {
+                if (previousZeroIndex != -1) {
+                    for (int j = previousZeroIndex + 1; j <= i; j++) {
+                        left = Math.abs(previousZeroIndex - j);
+                        right = Math.abs(i - j);
+                        stringBuilder.append(Math.min(left, right));
+                        stringBuilder.append(" ");
+                    }
+                } else {
+                    for (int j = 0; j <= i; j++) {
+                        stringBuilder.append(Math.abs(i - j));
+                        stringBuilder.append(" ");
+                    }
                 }
+                previousZeroIndex = i;
             }
-            if (!numbers[i].equals("0")) {
-                zeroDistance++;
-            }
-            if (numbers[i].equals("0") && previousZeroIndex > 0) {
-                for (int j = previousZeroIndex + 1; j < i; j++) {
-                    left = Math.abs(previousZeroIndex - j);
-                    right = Math.abs(i - j);
-                    stringBuilder.append(Math.min(left, right));
-                    stringBuilder.append(" ");
-                }
-                zeroDistance = 0;
-            }
-            if (i == housesNumber - 1 && previousZeroIndex > 0) {
-                for (int j = previousZeroIndex + 1; j <= i ; j++) {
-                    stringBuilder.append(Math.abs(i - previousZeroIndex));
+            if (i == housesNumber - 1) {
+                for (int j = previousZeroIndex + 1; j <= i; j++) {
+                    stringBuilder.append(Math.abs(j - previousZeroIndex));
                     stringBuilder.append(" ");
                 }
             }
         }
-//
-//
-//        for (int i = 0; i < numbers.length; i++) {
-//            if (numbers[i].equals("0")) {
-//                zeroIndexes.add(i);
-//            }
-//        }
-//
-//        for (int i = 0; i < numbers.length; i++) {
-//            if (numbers[i].equals("0")) {
-//                stringBuilder.append(0);
-//                stringBuilder.append(" ");
-//            } else {
-//                int diff = Math.abs(i - zeroIndexes.get(0));
-//                for (Integer zeroIndex : zeroIndexes) {
-//                    if (Math.abs(i - zeroIndex) < diff) {
-//                        diff = Math.abs(i - zeroIndex);
-//                    }
-//                }
-//                stringBuilder.append(diff);
-//                stringBuilder.append(" ");
-//            }
-//        }
         var result = stringBuilder.toString();
         return result.substring(0, result.length() - 1);
     }
