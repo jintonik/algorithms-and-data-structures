2c5a451d61a2ced060bfba67202efef8b65f9f75
Anton Sherpaev <asherpaev@odin.com>
2023-11-16 23:29:54 +0700

    sprint 2 B done

---
+++ app/src/main/java/algorithms/sprint2/B/Solution.java
@@ -0,0 +1,40 @@
+package algorithms.sprint2.B;
+
+// <template>
+class Node<V> {
+    public V value;
+    public Node<V> next;
+
+    public Node(V value, Node<V> next) {
+        this.value = value;
+        this.next = next;
+    }
+}
+// <template>
+
+public class Solution {
+    public static void solution(Node<String> head) {
+
+        for (int i = 0; i < 5000; i++) {
+            if (head != null) {
+                System.out.println(head.value);
+                head = head.next;
+            }
+        }
+    }
+
+    public static void test() {
+        Node<String> node3 = new Node<>("node3", null);
+        Node<String> node2 = new Node<>("node2", node3);
+        Node<String> node1 = new Node<>("node1", node2);
+        Node<String> node0 = new Node<>("node0", node1);
+        solution(node0);
+        /*
+        Output is:
+        node0
+        node1
+        node2
+        node3
+        */
+    }
+}
---
+++ app/src/test/java/algorithms/sprint2/BTest.java
@@ -0,0 +1,11 @@
+package algorithms.sprint2;
+
+import algorithms.sprint2.B.Solution;
+import org.junit.jupiter.api.Test;
+
+public class BTest {
+    @Test
+     public void test() {
+         Solution.test();
+     }
+}
